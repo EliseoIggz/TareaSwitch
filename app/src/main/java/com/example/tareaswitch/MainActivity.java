98132e6f90957d716466d836bc9f013ceae20fcf
EliseoIggz <eliseoiggz@gmail.com>
2024-10-30 18:44:22 +0100

    ewrwe

--- app/src/main/java/com/example/tareaswitch/MainActivity.java
+++ app/src/main/java/com/example/tareaswitch/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.tareaswitch;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -19,6 +25,25 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        SwitchCompat sB = findViewById(R.id.switch1);
+        ImageView myImageView = findViewById(R.id.imageView2);
+
+        sB.setTextOn("Visible");
+        sB.setTextOff("Invisible");
+
+        sB.setChecked(false);
+
+        sB.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b){
+                if (b){
+                    myImageView.setVisibility(View.VISIBLE);
+                }else{
+                    myImageView.setVisibility(View.GONE);
+                }
+            }
         });
     }
 }
